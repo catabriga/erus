f4098051384f259f82efdfa66af581fac1c5ce1e
juan fr <juan.franca@gmail.com>
2013-07-04 17:17:58 -0300

    Usado na competicao

--- TUR/ErusBot/ErusBot.cpp
+++ TUR/ErusBot/ErusBot.cpp
@@ -5,19 +5,45 @@
 #include "PIDControl.h"
 #include "Error.h"
 #include "LDR.h"
+#include "Leds.h"
 
-static int defaultVelocity = 180;
-static int turnVelocity1 = 90;
-static int turnVelocity2 = 140;
-static unsigned long lastTime;
+#define STOP_TIME 14000
+
+static int defaultVelocity = 190;
+static int turnVelocity1 = 150;
+static int turnVelocity2 = 170;
+
+static int leftSpeed = 170;
+static int rightSpeed = 170;
+static int curveCounter = 0;
+
+static unsigned long initialTime;
+static unsigned long currentTime;
+
+static int initialLight = 0;
+static long defaultLight;
+
+static bool inTunnel = false;
+static bool lastMoment = false;
+
+static int errorCount = 0;
+
+static int rightSensor;
+static int leftSensor;
+
+static int checkPoints = 0;
 
 void setup(void)
 {
 	Serial.begin(9600);
 	setupSensors();
 	setupMotors();
-	setupPIDControl(25, 0, 1);
+	setupPIDControl(0.7, 0, 0);
 	pinMode(BUTTON, INPUT);
+	pinMode(SIGNAL_LED, OUTPUT);
+
+	
+	defaultLight = getDefaultLight();
 }
 
 int checkInit(void)
@@ -28,7 +54,8 @@ int checkInit(void)
 		if(digitalRead(BUTTON))
 		{
 			init = 1;
-			delay(2000);
+            delay(1800);
+            
 		}
 	}
 	
@@ -37,62 +64,78 @@ int checkInit(void)
 
 void loop(void)
 {
+           
 	if(!checkInit())
 	{
 		return;
 	}
-	
+    
 	int* sensors = readSensors();
 	int error = getError(sensors);
+	int* leds = readLeds();
 	int ldr = getLDR();
-//	int pid = getPIDControl(error);
-	
-/*	setMotor(0, defaultVelocity + pid, 1); // Motor 0 -> esquerda
-	setMotor(1, defaultVelocity - pid, 1);
-	
-	Serial.print("Motor 0 = ");
-	Serial.println(defaultVelocity + pid);
-	Serial.print("Motor 1 = ");
-	Serial.println(defaultVelocity - pid);*/
-	
-	/*Serial.print(sensors[0]);
-	Serial.print(" / ");
-	Serial.print(sensors[1]);
-	Serial.print(" / ");
-	Serial.print(sensors[2]);
-	Serial.print(" / ");
-	Serial.print(sensors[3]);
-	Serial.print(" / ");
-	Serial.print(sensors[4]);
-	Serial.println(" / ");
-	Serial.println(error);*/
 
-	
-	
-	if(sensors[2] == 1)
-	{
-		setMotor(0, defaultVelocity, 1); // 1 - > Frente
-		setMotor(1, defaultVelocity, 1);
+	rightSensor = analogRead(RIGHT_SENSOR);
+	leftSensor = analogRead(LEFT_SENSOR);
+
+    if (ldr > 3 * defaultLight){
+    
+           inTunnel = true;
+           digitalWrite(SIGNAL_LED,HIGH);
+           
+    }else if (inTunnel == true && ldr < 1.5 * defaultLight){
+        initialTime = millis();
+        inTunnel = false;
+        lastMoment = true;
+        digitalWrite(SIGNAL_LED,LOW);
 	}
-	else if(error < 0)
-	{
+	
+
+    if ( millis() - initialTime > STOP_TIME && lastMoment == true){
+        setMotor(0,0,1);
+        setMotor(1,0,1);
+        
+        return;
+    }
+
+    if(sensors[2] == 1 && sensors[0] == 0 && sensors[4] == 0){
+		setMotor(0, defaultVelocity, 1); 
+		setMotor(1, defaultVelocity, 1);
+		
+	}else if (sensors[0] == 1 && sensors[1] == 1 && sensors[4] == 0){ 
+
+	    setMotor(0, defaultVelocity, 1); 
+		setMotor(1, defaultVelocity, 0);
+
+
+	}else if (sensors[4] == 1 && sensors[3] == 1 && sensors[0] == 0){
+
+		setMotor(1, defaultVelocity, 1); 
+		setMotor(0, defaultVelocity, 0);
+		
+	}else if(error < 0){
+	
 		if(sensors[0] == 0)
 		{
 			setMotor(0, turnVelocity1, 1);
 			setMotor(1, turnVelocity1, 0);
+
 		}
 		else
 		{
-			setMotor(0, turnVelocity2, 1);
+			setMotor(0, turnVelocity2, 1); 
 			setMotor(1, turnVelocity2, 0);
 		}
 	}
-	else if (error > 0)
-	{
+	else if (error > 0){
+	    
 		if(sensors[4] == 0)
 		{
 			setMotor(0, turnVelocity1, 0);
 			setMotor(1, turnVelocity1, 1);
+
+			errorCount = errorCount + 1;
+
 		}
 		else
 		{
@@ -101,9 +144,10 @@ void loop(void)
 		}
 	}
 	else
-	{
-	
+	{	
 		setMotor(0, defaultVelocity, 1); // 1 - > Frente
 		setMotor(1, defaultVelocity, 1);
+
+		errorCount = 0;
 	}
 }
