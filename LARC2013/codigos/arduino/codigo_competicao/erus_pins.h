ae65c5c2c03300d7d295efedd4e980be87ed3cfe
pet <pet@pet-note.(none)>
2013-10-11 20:06:18 -0300

    Atualizando o codigo do arduino

---
+++ LARC2013/codigos/arduino/codigo_competicao/erus_pins.h
@@ -0,0 +1,17 @@
+#ifndef ERUS_PINS_H
+#define ERUS_PINS_H
+
+//Pinos dos Motores para Movimentação
+#define DIR_R 46 //Direita
+#define PWM_R 9 
+#define DIR_L 48 //Esquerda
+#define PWM_L 10
+
+//Pinos do Motor da Vassoura
+#define DIR_VAS 34
+#define PWM_VAS 11
+
+#define PIN_VIB 23 //VIBRADOR
+#define PIN_SERVO 2
+
+#endif
---
+++ LARC2013/codigos/arduino/codigo_competicao/protocol.h
@@ -0,0 +1,10 @@
+#ifndef PROTOCOL_H
+#define PROTOCOL_H
+
+#define MOTOR_D 0X11
+#define MOTOR_E 0X12
+#define MOTOR_VAS 0X13
+#define SERVO 0x14
+
+
+#endif
