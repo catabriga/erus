3965a5a188ac4681244d8a024e4feb4c34e06f88
Lucas Catabriga <catabriga90@gmail.com>
2013-10-17 19:45:44 -0300

    Corrigido direcao perseguir lata

--- LARC2013/codigos/codigo_android/RobotBrain.java
+++ LARC2013/codigos/codigo_android/RobotBrain.java
@@ -15,7 +15,7 @@ public class RobotBrain
 	
 	private static final int LIMIT_MOTOR_MOVEMENT = 255;
 	
-	public static final double ROBOT_CENTER_OFFSET = 0.05;
+	public static final double ROBOT_CENTER_OFFSET = 0.0;
 	public static final int CATCHABLE_CAN_LIMIT_Y_MIN = 10;
 	public static final int CATCHABLE_CAN_LIMIT_Y_MAX = 30;
 	public static final int CATCHABLE_CAN_LIMIT_X = 40;
